4676a8ac49de1e17bf0587d7e59704813cfdb220
Zoey B <zoey.bonaventura@gmail.com>
2020-08-28 04:16:05 -0400

    Converted CSS to use rem

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,60 @@
 <template>
   <div class="home">
     <div class="header">
-      Section header
+      Header
     </div>
     <div class="panel">
       <div class="panel-header">
-        Section header
+        Header
       </div>
       <VSwitch v-model="enabled" :label="enabled ? 'On' : 'Off'"></VSwitch>
-      <VBtn large color="primary" @click.native="enabled = true">On</VBtn>
-      <p />
-      <VBtn large color="secondary" @click.native="enabled = false">Off</VBtn>
+      <VBtn class="square" small :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+      <VBtn small :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
     </div>
     <div class="panel">
       <VSwitch v-model="enabled" :label="enabled ? 'On' : 'Off'"></VSwitch>
-      <VBtn large color="primary" @click.native="enabled = true">On</VBtn>
-      <p />
-      <VBtn large color="secondary" @click.native="enabled = false">Off</VBtn>
+      <VBtn class="square" medium :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+      <VBtn medium :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
     </div>
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />x
-    <p />
-    <VBtn large color="accent">Accent</VBtn>
+    <div class="panel">
+      <VSwitch v-model="enabled" :label="enabled ? 'On' : 'Off'"></VSwitch>
+      <VBtn class="square" large :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+      <VBtn large :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+    </div>
+    <div class="panel">
+      <VSwitch v-model="enabled" :label="enabled ? 'On' : 'Off'"></VSwitch>
+      <VBtn class="square" x-large :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+      <VBtn x-large :color="enabled ? 'primary' : 'secondary'" @click.native="enabled = !enabled"></VBtn>
+    </div>
+    <div class="panel">
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />x
+      <p />
+      <VBtn large color="accent">Accent</VBtn>
+    </div>
   </div>
 </template>
 <script>
