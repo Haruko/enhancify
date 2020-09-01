aef8ef26d19df0d29633c260b0ed6138236ce81c
Zoey B <zoey.bonaventura@gmail.com>
2020-09-01 00:15:18 -0400

    Added expansion panels

--- src/components/BookmarksPanel/BookmarksPanel.vue
+++ src/components/BookmarksPanel/BookmarksPanel.vue
@@ -11,7 +11,7 @@
       </VRow>
       <VRow no-gutters align="center">
         <VCol class="offset-sm-1" cols="5">
-          <VSwitch v-model="allowDupesLocal" :disabled="!saveBookmarksLocal" dense hide-details="true" label="Allow duplicates"></VSwitch>
+          <VSwitch v-if="saveBookmarksLocal" v-model="allowDupesLocal" :disabled="!saveBookmarksLocal" dense hide-details="true" label="Allow duplicates"></VSwitch>
         </VCol>
         <VCol cols="6">
           <VBtn color="primary" small @click.native="openBookmarksDir">Open bookmarks directory</VBtn>
@@ -27,7 +27,7 @@
       </VRow>
       <VRow no-gutters align="center">
         <VCol class="offset-sm-1" cols="5">
-          <VSwitch v-model="allowDupesSpotify" :disabled="!saveBookmarksSpotify" dense hide-details="true" label="Allow duplicates"></VSwitch>
+          <VSwitch v-if="saveBookmarksSpotify" v-model="allowDupesSpotify" :disabled="!saveBookmarksSpotify" dense hide-details="true" label="Allow duplicates"></VSwitch>
         </VCol>
       </VRow>
     </VCol>
--- src/views/Authorized.vue
+++ src/views/Authorized.vue
@@ -1,31 +1,77 @@
 <template>
+  <!-- <VContainer>
+      <VRow class="header">
+        <VCol>
+          Config
+        </VCol>
+      </VRow>
+      <ConfigPanel />
+      <VRow class="header">
+        <VCol>
+          Now Playing Labels
+        </VCol>
+      </VRow>
+      <FileFormatsPanel />
+      <VRow class="header">
+        <VCol>
+          Bookmarks
+        </VCol>
+      </VRow>
+      <BookmarksPanel />
+      <VRow class="header">
+        <VCol>
+          Debug
+        </VCol>
+      </VRow>
+      <DebugPanel />
+  </VContainer> -->
   <VContainer>
-    <VRow class="header">
-      <VCol>
-        Config
-      </VCol>
-    </VRow>
-    <ConfigPanel />
-    <VRow class="header">
-      <VCol>
-        Now Playing Labels
-      </VCol>
-    </VRow>
-    <FileFormatsPanel />
-    <VRow class="header">
-      <VCol>
-        Bookmarks
-      </VCol>
-    </VRow>
-    <BookmarksPanel />
-    <VRow class="header">
-      <VCol>
-        Debug
-      </VCol>
-    </VRow>
-    <DebugPanel />
+    <VExpansionPanels v-model="panels" multiple flat accordion>
+      <VExpansionPanel>
+        <VExpansionPanelHeader class="header">
+          Config
+        </VExpansionPanelHeader>
+        <VExpansionPanelContent color="#00000000">
+          <ConfigPanel />
+        </VExpansionPanelContent>
+      </VExpansionPanel>
+      <VExpansionPanel>
+        <VExpansionPanelHeader class="header">
+          Now Playing Labels
+        </VExpansionPanelHeader>
+        <VExpansionPanelContent color="#00000000">
+          <FileFormatsPanel />
+        </VExpansionPanelContent>
+      </VExpansionPanel>
+      <VExpansionPanel>
+        <VExpansionPanelHeader class="header">
+          Bookmarks
+        </VExpansionPanelHeader>
+        <VExpansionPanelContent color="#00000000">
+          <BookmarksPanel />
+        </VExpansionPanelContent>
+      </VExpansionPanel>
+      <VExpansionPanel>
+        <VExpansionPanelHeader class="header">
+          Debug
+        </VExpansionPanelHeader>
+        <VExpansionPanelContent color="#00000000">
+          <DebugPanel />
+        </VExpansionPanelContent>
+      </VExpansionPanel>
+    </VExpansionPanels>
   </VContainer>
 </template>
+<style>
+.v-expansion-panel {
+  background-color: rgba(0, 0, 0, 0) !important;
+}
+
+.v-expansion-panel-header,
+.v-expansion-panel-content__wrap {
+  padding: 0px !important;
+}
+</style>
 <script>
 import ConfigPanel from '@/components/ConfigPanel/ConfigPanel.vue';
 import FileFormatsPanel from '@/components/FileFormatsPanel/FileFormatsPanel.vue';
@@ -40,6 +86,12 @@ export default {
     FileFormatsPanel,
     BookmarksPanel,
     DebugPanel,
+  },
+
+  data() {
+    return {
+      panels: [0, 1, 2],
+    };
   },
 }
 </script>
